632df3f5f8af6a3d6627f15e771e707a60539110
Taesueng Lee <taeseunglee159@gmail.com>
2018-07-09 22:31:30 -0700

    Add bootstrap example html and a route about html

--- web-server/web.py
+++ web-server/web.py
@@ -1,20 +1,24 @@
-from flask import Flask
-from flask import Flask, request, redirect, url_for
+import os
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route('/')
+def homepage():
+    title = "Epic Tutorials"
+    try:
+        return render_template("example_bootstrap.html")
+    except Exception as e:
+        return "Exception occur " + str(e)
+
+@app.route('/hello')
 def hello_world():
     return 'Hello World'
 
-@app.route('/bye')
-def bye_func():
-    return '''
-    <!doctype html>
-    <title>Bye</title>
-    <h1>BYE</h1>
-    '''
+@app.route('/a')
+def a_page():
+    return render_template("a.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001', debug=True)
